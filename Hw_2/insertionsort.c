91f250477ad5f79eccde4664c7ff461dc04c9076
Karla Barraza <kbarra2@pdx.edu>
2018-10-16 12:13:44 -0700

    adding files for insertion sort

---
+++ Hw_2/insertionsort.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "insertionsort.h"
+
+int insertionsort(int arr[], int size, int * ncompares_ptr, int *nswaps_ptr){
+
+
+   return 0;
+}
---
+++ Hw_2/insertionsort.h
@@ -0,0 +1,11 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+
+
+
+
+#ifndef INSERTIONSORT_H
+#define INSERTIONSORT_H
+   int insertionsort(int arr[], int size, int *ncompares_ptr, int *nswaps_ptr);
+   #define IS_Verbose
+#endif
